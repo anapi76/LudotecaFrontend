f2853a155f9fed81dc334a5d0f4ac54804bf32b8
anapi76 <abpj76@gmail.com>
2024-11-28 16:23:18 +0100

    Conecto customer con el backend

--- src/app/customer/customer-edit/customer-edit.component.ts
+++ src/app/customer/customer-edit/customer-edit.component.ts
@@ -2,20 +2,22 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { Customer } from '../model/Customer';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { CustomerService } from '../customer.service';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatError, MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-customer-edit',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule,MatButtonModule, MatInputModule, MatFormFieldModule],
+  imports: [FormsModule, ReactiveFormsModule, MatButtonModule, MatInputModule, MatFormFieldModule, CommonModule, MatError],
   templateUrl: './customer-edit.component.html',
   styleUrl: './customer-edit.component.scss'
 })
 export class CustomerEditComponent implements OnInit {
   customer: Customer;
+  nameAlreadyExists: Boolean = false;
 
   constructor(
     public dialogRef: MatDialogRef<CustomerEditComponent>,
@@ -28,9 +30,19 @@ export class CustomerEditComponent implements OnInit {
   }
 
   onSave() {
-    this.customerService.saveCustomer(this.customer).subscribe(() => {
-      this.dialogRef.close();
-    });
+    if(this.customer.name){
+      this.customerService.getCustomer(this.customer.name).subscribe((existingCustomer) => {
+        if (existingCustomer) {
+          this.nameAlreadyExists = true;
+          this.customer.name = ''; 
+        } else {
+          this.nameAlreadyExists = false;
+          this.customerService.saveCustomer(this.customer).subscribe(() => {
+            this.dialogRef.close();
+          })
+        }
+      });
+    }
   }
 
   onClose() {
--- src/app/customer/customer.service.ts
+++ src/app/customer/customer.service.ts
@@ -1,24 +1,34 @@
 import { Injectable } from '@angular/core';
-import { CUSTOMER_DATA } from './model/mock-customers';
+//import { CUSTOMER_DATA } from './model/mock-customers';
 import { Observable, of } from 'rxjs';
 import { Customer } from './model/Customer';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  private baseUrl = 'http://localhost:8080/customer';
 
   getCustomers():Observable<Customer[]>{
-    return of(CUSTOMER_DATA);
+    return this.http.get<Customer[]>(this.baseUrl);
+  }
+
+  getCustomer(name:string):Observable<Customer|null>{
+    const url = `${this.baseUrl}/${name}`;
+    return this.http.get<Customer|null>(url);
   }
 
   saveCustomer(customer:Customer):Observable<Customer>{
-    return of(null);
+    const { id } = customer;
+    const url = id ? `${this.baseUrl}/${id}` : this.baseUrl;
+    return this.http.put<Customer>(url, customer);
   }
 
   deleteCustomer(idCustomer:number):Observable<any>{
-    return of(null);
+    return this.http.delete<void>(`${this.baseUrl}/${idCustomer}`);
   }
 }
